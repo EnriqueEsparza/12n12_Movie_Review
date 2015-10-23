ffcea0c254c8cab914a169d09a50c524b204bfa7
enrique <iqueart@yahoo.com>
2015-10-23 18:23:16 -0500

    revised s3 info in movie model

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -3,11 +3,11 @@ class Movie < ActiveRecord::Base
   belongs_to :user
   has_many :reviews
 
-  has_attached_file :image, styles: { medium: "400x600#" }
-  validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
+  has_attached_file :image, styles: { medium: "400x600#" },
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/ ,
 
-  :default_url => "#{Rails.root}/app/assets/images/missing.jpg",
-    :storage => :s3,
-    :s3_credentials => "#{Rails.root}/config/environments/production.rb",
-    :path => "/movie_images/:style/:id/:filename"
+    storage: :s3,
+    s3_credentials: "#{Rails.root}/config/s3.yml",
+    path: ":attachment/:id/:style.:extension"
+    bucket: "movie-review"
 end
