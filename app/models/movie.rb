fc16a5e62fa35322cdb3f8335e34ad2ab32ef975
enrique <iqueart@yahoo.com>
2015-11-05 14:52:56 -0600

    updated readme.md

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -3,6 +3,11 @@ class Movie < ActiveRecord::Base
   belongs_to :user
   has_many :reviews
 
-  has_attached_file :image, styles: { medium: "400x600#>" }
+  has_attached_file :image, styles: { medium: "400x600#" },
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
+
+    storage: :s3,
+    s3_credentials: "#{Rails.root}/config/s3.yml"
+    path: ":attachment/:id/:style.:extension"
+    bucket: "movie-review"
 end
